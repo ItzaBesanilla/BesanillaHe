66a139090c87528b253b53a608c49f78229024ab
ItzaBesanilla <l18100153@nlaredo.tecnm.mx>
2021-06-10 10:54:02 -0600

    Actualización

---
+++ Parcial 3/LoginDB/DBusuario.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS besanillahe; 
+use besanillahe;
+Create table Usuario
+( idUsuario smallint not null auto_increment,
+  nombre    varchar(30),
+  Usuario varchar(20),
+  contraseña varchar(40),
+ 
+  PRIMARY KEY(idUsuario)
+);
+
+insert into Usuario (nombre, Usuario, contraseña)
+values ('Itzamara Besanilla Hdz','itzamara','0f1dbdede245f84e02c17da51c2c2dd3'), -- besanilla
+('Eduardo Amador','Eduardo','299ad925b432e99fbc250cf7fde69c51'); -- amador
+
+select * from Usuario;
---
+++ Parcial 3/LoginDB/Password.php
@@ -0,0 +1,8 @@
+<?php
+
+echo md5('besanilla');   // 0f1dbdede245f84e02c17da51c2c2dd3
+echo '<br>';
+
+echo md5('amador'); //299ad925b432e99fbc250cf7fde69c51
+echo '<br>';
+?>
