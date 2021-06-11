6d094755646aa3a0d060e43ddf0d99b09903f30e
ItzaBesanilla <l18100153@nlaredo.tecnm.mx>
2021-06-11 00:37:53 -0600

    Actualización

--- Parcial 3/LoginDB/DBusuario.sql
+++ Parcial 3/LoginDB/DBusuario.sql
@@ -4,11 +4,12 @@ Create table Usuario
 ( idUsuario smallint not null auto_increment,
   nombre    varchar(30),
   Usuario varchar(20),
-  contraseña varchar(40),
+  contrasena varchar(40),
  
   PRIMARY KEY(idUsuario)
 );
 
+alter table usuario change contraseña contrasena varchar(40);
 insert into Usuario (nombre, Usuario, contraseña)
 values ('Itzamara Besanilla Hdz','itzamara','0f1dbdede245f84e02c17da51c2c2dd3'), -- besanilla
 ('Eduardo Amador','Eduardo','299ad925b432e99fbc250cf7fde69c51'); -- amador
